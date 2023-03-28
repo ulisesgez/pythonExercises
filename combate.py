b7aa57ec1ef50b62e9d4a090cf9286a610b5de44
ulisesGutierrez <techwithulises@gmail.com>
2023-03-28 13:43:32 -0600

    correccionCombate

--- combate.py
+++ combate.py
@@ -1,28 +1,25 @@
 from random import randint
 
 vidaUno = 100
-vidaDos = 90
+vidaDos = 100
 
 while vidaUno > 0 and vidaDos > 0:
     #primer peleador:
     print('ataque primer peleador')
-    #genera numero entre 1 y dos:
-    ataqueUno = randint(1, 2)
-    if ataqueUno == 1:
-        print('ataque')
-        vidaDos -= 10
-    else:
-        print('ataque')
-        vidaDos -= 10
+    #randint genera numero entre 1 y 10:
+    ataqueUno = randint(1, 10)
+    vidaDos -= ataqueUno
     
     #segundo peleador:
     print('ataque segundo peleador')
-    ataqueDos = None
-    while ataqueDos != 'a' and ataqueDos != 'b' and ataqueDos != 'c':
-        ataqueDos = input('ataque a realizar: a)golpe, b)patada, c)tirada: ')
+    seleccion = None
+    while seleccion != 'a' and seleccion != 'b' and seleccion != 'c':
+        seleccion = input('ataque a realizar: a)golpe, b)patada, c)tirada: ')
 
-    if ataqueDos == 'a'or 'b' or 'c':
-        vidaUno -= 10
+    if seleccion == 'a'or 'b' or 'c':
+        ataqueDos = randint(1, 10)
+        print('ataque segundo peleador')
+        vidaUno -= ataqueDos
 
     print(f'vida p1: {vidaUno} y p2: {vidaDos}')
 
