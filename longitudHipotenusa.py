2d4ddbf3179526de7bd2379d525028acb5ec1268
ulisesGutierrez <techwithulises@gmail.com>
2023-05-11 21:11:24 -0600

    new

---
+++ longitudHipotenusa.py
@@ -0,0 +1,7 @@
+"""
+El siguiente código evalúa la longitud de la hipotenusa (es decir, el lado más largo de un triángulo rectángulo, el opuesto al ángulo recto) usando el teorema de Pitágoras:
+"""
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
---
+++ manzanas.py
@@ -0,0 +1,28 @@
+"""
+A continuación una historia:
+
+Érase una vez en la Tierra de las Manzanas, Juan tenía tres manzanas, María tenía cinco manzanas, y Adán tenía seis manzanas. Todos eran muy felices y vivieron por muchísimo tiempo. Fin de la Historia.
+
+Tu tarea es:
+
+Crear las variables: john, mary, y adam;
+Asignar valores a las variables. El valor debe de ser igual al número de manzanas que cada quien tenía;
+Una vez almacenados los números en las variables, imprimir las variables en una línea, y separar cada una de ellas con una coma;
+Después se debe crear una nueva variable llamada total_apples y se debe igualar a la suma de las tres variables anteriores;
+Imprime el valor almacenado en total_apples en la consola;
+Experimenta con tu código: crea nuevas variables, asigna diferentes valores a ellas, y realiza varias operaciones aritméticas con ellas (por ejemplo, +, -, *, /, //, etc.). Intenta poner una cadena con un entero juntos en la misma línea, por ejemplo, "Número total de manzanas:" y total_apples.
+"""
+
+john = 3
+mary = 5
+adam = 6
+
+print(john, mary, adam, sep=',')
+
+total_apples = john + mary + adam
+print(total_apples)
+
+# peter = 12.5
+# suzy = 2
+# print(peter / suzy)
+# print("Total number of apples:", total_apples)
