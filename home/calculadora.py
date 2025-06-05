c20f98256b8d1128a0028d9d421f5de84bdfb93d
UlisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-06-05 09:50:43 -0600

    clean

--- home/calculadora.py
+++
@@ -1,6 +0,0 @@
-numeroUno = input("Ingresa primer numero: ")
-numeroDos = input("Ingresa segundo numero: ")
-
-numeroUno = int(numeroUno)
-numeroDos = int(numeroDos)
-print(numeroUno + numeroDos)
--- home/numero_tres.py
+++
@@ -1,6 +0,0 @@
-#A traves de PAMDAS, hacer que la siguiente expresion sea igual
-# a 3, ya que actualmente es igual a 7:
-#print(3*3+3/3-3)
-
-print(3*(3+3)/3-3)
-print((3+3*3-3)/3)
