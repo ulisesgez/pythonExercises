30daf0df1a37109a7b20864c8a2ef44fc56ab0bb
UlisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-06-06 10:35:36 -0600

    excersiseOne

---
+++ home/day1.py
@@ -0,0 +1,13 @@
+"""
+Printing Practice
+Write a program that uses print statements to print the following recipe into the Output console.
+The text to print is already there, you just need to make it into code.
+Your code should print all five lines exactly the same as the example output below.
+Make sure that you don't change any of these existing text as everything, punctuation and casing all need to match!
+"""
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
