36f9d6febf66892a1ecf97d41509dfdaa7b45a86
UlisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-06-06 10:39:48 -0600

    differentForms

--- home/day1.py
+++ home/day1.py
@@ -9,5 +9,6 @@ print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
 print("2. Knead the dough for 10 minutes.")
 print("3. Add 3g of Salt.")
 print("4. Leave to rise for 2 hours.")
-print("5. Bake at 200 degrees C for 30 minutes.")
+print("5. Bake at 200 degrees C for 30 minutes.\n")
 
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
