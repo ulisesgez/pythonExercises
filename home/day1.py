22036d09768c138516d008162b991b209fbf17fb
UlisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-06-13 11:01:41 -0600

    variables

--- home/day1.py
+++ home/day1.py
@@ -14,3 +14,12 @@ print("5. Bake at 200 degrees C for 30 minutes.\n")
 print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
 
 print("Hello " + input("What is your name? ") + "!")
+
+print(len("luis"))
+
+"""
+Variables:
+We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. Write 3 lines of code to
+switch the contents of the variables. You are not allowed to type the words "milk" or "juice". You are only
+allowed to use variables to solve this exercise.
+"""
